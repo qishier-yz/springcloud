9740608421a4f46e9ab22ec2a5da4c032cabf2c4
weimeng2017 <weimeng2017@cnpc.com.cn>
2019-04-09 15:58:46 +0800

    增加@EnableDiscoveryClient和@RefreshScope，以便刷新配置

--- config-server/src/main/java/com/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/server/ConfigServerApplication.java
@@ -2,12 +2,16 @@ package com.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableConfigServer
+@RefreshScope
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
