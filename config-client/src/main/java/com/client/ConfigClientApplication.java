bd908a9ecd5b58ea36b5226ebed7690966f62e67
weimeng2017 <weimeng2017@cnpc.com.cn>
2019-04-09 15:57:55 +0800

    增加@RefreshScope标签，在配置中心总线刷新后，以便应用刷新配置

--- config-client/src/main/java/com/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/client/ConfigClientApplication.java
@@ -3,6 +3,8 @@ package com.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
