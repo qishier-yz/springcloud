3950345f04b9f4c7eb50b565ed1ba45d37632988
weimeng2017 <weimeng2017@cnpc.com.cn>
2019-04-10 16:47:05 +0800

    链路追踪

---
+++ server-zipkin/src/main/java/zipkin/server/ServerZipkinApplication.java
@@ -0,0 +1,13 @@
+package zipkin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerZipkinApplication.class, args);
+	}
+
+}
