b7dd8230c608901ca1d7930b0d54636294aebb5d
weimeng2017 <weimeng2017@cnpc.com.cn>
2019-04-10 17:08:33 +0800

    增加断路器

--- service-gateway/src/main/java/service/gateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/service/gateway/ServiceGatewayApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @RestController
@@ -17,10 +19,29 @@ public class ServiceGatewayApplication {
 
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder){
-		return  builder.routes()
+		/*return  builder.routes()
 				.route(p -> p.path("/get")
 				.filters(f -> f.addRequestHeader("hello","world"))
 				.uri("http://httpbin.org/get"))
-				.build();
+				.build();*/
+
+		String httpUri = "http://httpbin.org:80";
+		return builder.routes().route(p -> p
+                .path("/get")
+                .filters(f -> f.addRequestHeader("",""))
+                .uri(httpUri))
+                .route(p -> p
+                .host("*.hystrix.com")
+                .filters(f -> f
+                    .hystrix(config -> config
+                        .setName("mycmd")
+                        .setFallbackUri("forward:/fallback")))
+                .uri(httpUri))
+                .build();
 	}
+
+	@RequestMapping("/fallback")
+    public Mono<String> fallback(){
+	    return  Mono.just("fallback");
+    }
 }
