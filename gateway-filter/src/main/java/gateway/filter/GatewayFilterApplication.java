f84a364f72cd1156404c213a7d8a37dcf794db07
weimeng2017 <weimeng2017@cnpc.com.cn>
2019-04-17 15:48:09 +0800

    gatewayfilter

---
+++ gateway-filter/src/main/java/gateway/filter/GatewayFilterApplication.java
@@ -0,0 +1,15 @@
+package gateway.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayFilterApplication.class, args);
+	}
+
+}
